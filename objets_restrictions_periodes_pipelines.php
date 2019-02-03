027a53f38b1f286a7a9e1d69f01215582d1fe64e
Rainer Müller <rainer@websimple.be>
2019-02-03 18:01:06 +0100

    lam pipeline, à retravailler

--- objets_restrictions_periodes_pipelines.php
+++ objets_restrictions_periodes_pipelines.php
@@ -13,9 +13,38 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-
-/*
- * Un fichier de pipelines permet de regrouper
- * les fonctions de branchement de votre plugin
- * sur des pipelines existants.
+/**
+ * Déclare les champs extras pour le formulaire prix.
+ *
+ * @pipeline prix_objets_extensions
+ *
+ * @param array $flux
+ *          Données du pipeline
+ * @return array
  */
+function prix_objets_periodes_prix_objets_extensions($flux) {
+
+	$flux['data']['periode'] = array (
+		array(
+			'saisie' => 'periodes',
+			'options' => array(
+				'nom' => 'id_prix_extension_periode',
+				'label' => _T('po_periode:champ_id_prix_extension_po_periode'),
+				'option_intro' => _T('po_periode:info_aucun_po_periode'),
+				'defaut' => $flux['id_prix_extension_objet'],
+				'class' => 'chosen',
+				'multiple' => 'oui',
+			)
+		),
+		array(
+			'saisie' => 'ajouter_action',
+			'options' => array(
+				'nom' => 'ajouter_periode',
+				'label_action' => _T('periode:icone_creer_periode'),
+				'objet' => 'periode',
+			)
+		),
+	);
+
+	return $flux;
+}
